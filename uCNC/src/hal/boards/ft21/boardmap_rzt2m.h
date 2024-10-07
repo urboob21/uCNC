4ecd8d93bffd09d96cf597a94a16eabd2936753d
Phong Nguyen <nphong2103@gmail.com>
2024-10-07 10:09:18 +0700

    Add skeleton boardmap_rzt2m

---
+++ uCNC/src/hal/boards/ft21/boardmap_rzt2m.h
@@ -0,0 +1,28 @@
+/*
+    Name: mcumap_grbl.h
+    Description: Contains all MCU and PIN definitions for RZT2M to run µCNC.
+*/
+
+#ifndef BOARDMAP_RZT_H
+#define BOARDMAP_RZT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef MCU
+#define MCU MCU_MCU_RZT
+#endif
+
+#ifndef BOARD_NAME
+#define BOARD_NAME "RZT"
+#endif
+
+// Setup pins
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
