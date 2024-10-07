29b649afcdcf3ac0123c2cb49033f3609f832cc7
Phong Nguyen <nphong2103@gmail.com>
2024-10-07 15:02:57 +0700

    gpio mapping functions

--- uCNC/src/hal/mcus/rzt/mcu_rzt.c
+++ uCNC/src/hal/mcus/rzt/mcu_rzt.c
@@ -1,2 +1,17 @@
 
-	// Description: Implements the µCNC HAL for RZT2M.
+// Description: Implements the µCNC HAL for RZT2M.
+
+#include "../../../cnc.h"
+
+/**
+ * initializes the mcu
+ * this function needs to:
+ *   - configure all IO pins (digital IO, PWM, Analog, etc...)
+ *   - configure all interrupts
+ *   - configure uart or usb
+ *   - start the internal RTC
+ * */
+void mcu_init(void)
+{
+    // mcu_io_init();             R_IOPORT_Open(&g_ioport_ctrl, &g_bsp_pin_cfg);
+}
--- uCNC/src/hal/mcus/rzt/mcumap_rzt.h
+++ uCNC/src/hal/mcus/rzt/mcumap_rzt.h
@@ -3,6 +3,14 @@
 #ifndef MCUMAP_RZT_H
 #define MCUMAP_RZT_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    // Include
+
+#include "..\..\..\..\makefiles\rzt\rzt_gen\hal_data.h"
+
 // needed by software delays
 #ifndef MCU_CLOCKS_PER_CYCLE
 #define MCU_CLOCKS_PER_CYCLE 1
@@ -12,6 +20,30 @@
 #endif
 #ifndef MCU_CYCLES_PER_LOOP_OVERHEAD
 #define MCU_CYCLES_PER_LOOP_OVERHEAD 0
+#endif
+
+#define mcu_config_output(X) \
+    {                        \
+    }
+
+#define mcu_config_input(X) \
+    {                       \
+    }
+
+#define mcu_config_analog(X) \
+    {                        \
+    }
+
+#define mcu_config_pullup(X) \
+    {                        \
+    }
+
+#define mcu_config_input_isr(X) \
+    {                           \
+    }
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
