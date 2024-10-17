2c79e1e83d786dcbaaf6864f5ac1d5c93d716649
Phong Nguyen <nphong2103@gmail.com>
2024-10-17 14:57:49 +0700

    Add GPIO input pin

--- uCNC/src/hal/boards/ft21/boardmap_ft21.h
+++ uCNC/src/hal/boards/ft21/boardmap_ft21.h
@@ -19,9 +19,43 @@ extern "C"
 #define BOARD_NAME "RZT"
 #endif
 
-// Setup step pins
-// TODO: NEED TO UPDATE
+// Setup GPIO pins (Mode: Input)
+// #define GPIO_0 (BSP_IO_PORT_02_PIN_1)
+#define DIN0_BIT 1
+#define DIN0_PORT 2
+#define DIN0_ISR 0
 
+// #define GPIO_3 (BSP_IO_PORT_03_PIN_2)
+#define DIN3_BIT 3
+#define DIN3_PORT 2
+#define DIN3_ISR 0
+
+// #define GPIO_7 (BSP_IO_PORT_04_PIN_5)
+#define DIN7_BIT 5
+#define DIN7_PORT 4
+#define DIN7_ISR 0
+
+// #define GPIO_8 (BSP_IO_PORT_07_PIN_5)
+#define DIN8_BIT 7
+#define DIN8_PORT 5
+#define DIN8_ISR 0
+
+    // #define GPIO_15 (BSP_IO_PORT_13_PIN_5)
+    // #define GPIO_16 (BSP_IO_PORT_13_PIN_6)
+    // #define GPIO_17 (BSP_IO_PORT_13_PIN_7)
+    // #define GPIO_18 (BSP_IO_PORT_14_PIN_0)
+    // #define GPIO_19 (BSP_IO_PORT_14_PIN_1)
+    // #define GPIO_20 (BSP_IO_PORT_16_PIN_4)
+    // #define GPIO_21 (BSP_IO_PORT_17_PIN_0)
+    // #define GPIO_22 (BSP_IO_PORT_17_PIN_1)
+    // #define GPIO_23 (BSP_IO_PORT_19_PIN_0)
+    // #define GPIO_27 (BSP_IO_PORT_20_PIN_4)
+    // #define GPIO_28 (BSP_IO_PORT_21_PIN_6)
+    // TODO: need to update
+
+        // Setup step pins
+
+#define ONESHOT_TIMER 4
 #ifdef __cplusplus
 }
 #endif
