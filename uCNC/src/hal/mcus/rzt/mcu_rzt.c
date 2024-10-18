7e8ec392ac127692803edebcb5a5e23e7fd47c21
Phong Nguyen <nphong2103@gmail.com>
2024-10-18 11:37:56 +0700

    Add uart config skeleton

--- uCNC/src/hal/mcus/rzt/mcu_rzt.c
+++ uCNC/src/hal/mcus/rzt/mcu_rzt.c
@@ -4,6 +4,15 @@
 #include "../../../cnc.h"
 
 /**
+ *
+ * Initializes the mcu:
+ *   1. Configures all IO
+ *   2. Configures UART/USB
+ **/
+static void mcu_rtc_init(void);
+static void mcu_usart_init(void);
+
+/**
  * initializes the mcu
  * this function needs to:
  *   - configure all IO pins (digital IO, PWM, Analog, etc...)
@@ -13,8 +22,14 @@
  * */
 void mcu_init(void)
 {
-    // Configure 
-    mcu_io_init();  // The FSP generation almostly done this.
+    // Configure
+    mcu_io_init();    // The FSP generation almostly done this.
+    mcu_usart_init(); // UART init
+}
 
-    //
+void mcu_usart_init(void)
+{
+#ifdef MCU_HAS_UART
+// TODO: implement UART FSP config here
+#endif
 }
