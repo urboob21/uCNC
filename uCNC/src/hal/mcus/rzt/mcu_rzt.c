cadd3f6591367597d4eef6bfee337831db87f503
Phong Nguyen <nphong2103@gmail.com>
2024-10-18 11:39:55 +0700

    Add rtc skeleton

--- uCNC/src/hal/mcus/rzt/mcu_rzt.c
+++ uCNC/src/hal/mcus/rzt/mcu_rzt.c
@@ -8,6 +8,7 @@
  * Initializes the mcu:
  *   1. Configures all IO
  *   2. Configures UART/USB
+ *   3. Starts internal clock (RTC)
  **/
 static void mcu_rtc_init(void);
 static void mcu_usart_init(void);
@@ -25,6 +26,7 @@ void mcu_init(void)
     // Configure
     mcu_io_init();    // The FSP generation almostly done this.
     mcu_usart_init(); // UART init
+    mcu_rtc_init();   // RTC init
 }
 
 void mcu_usart_init(void)
@@ -32,4 +34,9 @@ void mcu_usart_init(void)
 #ifdef MCU_HAS_UART
 // TODO: implement UART FSP config here
 #endif
+}
+
+void mcu_rtc_init()
+{
+    // TODO: implement RTC FSP config here
 }
